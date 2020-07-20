6b11bd7422fa7e1fc32ce540cd3f4e320f7f31e7
duart38 <mr.codercuracao@gmail.com>
2020-07-20 18:06:31 +0200

    Added some logic to an example plugin

---
+++ plugins/titlePlugin.ts
@@ -0,0 +1,40 @@
+export function dummyTitle(length: number): string {
+  var words = [
+    "The sky",
+    "above",
+    "the port",
+    "was",
+    "the color of television",
+    "tuned",
+    "to",
+    "a dead channel",
+    ".",
+    "All",
+    "this happened",
+    "more or less",
+    ".",
+    "I",
+    "had",
+    "the story",
+    "bit by bit",
+    "from various people",
+    "and",
+    "as generally",
+    "happens",
+    "in such cases",
+    "each time",
+    "it",
+    "was",
+    "a different story",
+    ".",
+    "It",
+    "was",
+    "a pleasure",
+    "to",
+    "burn",
+  ];
+  var text = [];
+  var x = length;
+  while (--x) text.push(words[Math.floor(Math.random() * words.length)]);
+  return text.join(" ");
+}

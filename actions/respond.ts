ab1228fc5844c87693a0d5ec606e1dc3f26e3c55
duart38 <mr.codercuracao@gmail.com>
2020-07-20 14:06:55 +0200

    Added empty method for future response construction

---
+++ actions/respond.ts
@@ -0,0 +1,8 @@
+/**
+ * Constructs response data based on provided interface. 
+ * Note that this method does not check if the model is available (see router.ts)
+ * @param model interface to use to construct response data
+ */
+export function constructResponse(model:String){
+    
+}

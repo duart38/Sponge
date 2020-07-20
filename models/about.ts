70fc78a8fd9caf21df7b68877311327786e9c5f6
duart38 <mr.codercuracao@gmail.com>
2020-07-20 18:25:47 +0200

    Added integer plugin

--- models/about.ts
+++ models/about.ts
@@ -1,6 +1,8 @@
 import { dummyTitle } from "../plugins/titlePlugin.ts";
+import { dummyInteger } from "../plugins/numberPlugin.ts";
 
 export const About = {
     title: dummyTitle(10), // < plugins
-    description: "static stuff"
+    description: "static stuff",
+    some_number: dummyInteger(1, 20)
 }

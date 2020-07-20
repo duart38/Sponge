49c46b3ada794ce87d130e61a43cbeeb83096e5c
duart38 <mr.codercuracao@gmail.com>
2020-07-20 15:12:25 +0200

    Added boilerplate code for constructing a response

--- actions/respond.ts
+++ actions/respond.ts
@@ -1,8 +1,12 @@
+import {About} from "../models/about.ts";
+
 /**
  * Constructs response data based on provided interface. 
  * Note that this method does not check if the model is available (see router.ts)
  * @param model interface to use to construct response data
  */
-export function constructResponse(model:String){
-    
+export function constructResponse(model:String) : object{
+   console.log(About.body);
+   console.log(About["body"]);
+   return {};
 }

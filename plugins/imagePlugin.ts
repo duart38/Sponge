1a9101486a9a8108b3603367b6619a2a9ccd86f9
duart38 <mr.codercuracao@gmail.com>
2020-07-20 19:13:33 +0200

    Added image plugin

---
+++ plugins/imagePlugin.ts
@@ -0,0 +1,7 @@
+/**
+ * Generates a square image (width and height are the same)
+ * @param size 
+ */
+export function dummySquareImage(size: number = 150){
+    return "https://via.placeholder.com/"+size;
+}

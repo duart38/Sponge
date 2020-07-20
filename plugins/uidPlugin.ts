bb6a13a5d47df1bf6ede2e274733ba7b0c70e236
duart38 <mr.codercuracao@gmail.com>
2020-07-20 22:00:12 +0200

    Added plugin to generate unique IDs

---
+++ plugins/uidPlugin.ts
@@ -0,0 +1,7 @@
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
+/**
+ * Generates a unique ID using (https://deno.land/std/uuid/mod.ts)
+ */
+export function uid(){
+    return v4.generate();
+}

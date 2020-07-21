c5cf2e9394cbdc194ed8289676111eb614c199ff
duart38 <mr.codercuracao@gmail.com>
2020-07-21 13:02:58 +0200

    Added a plugin to replicate an object or model

---
+++ plugins/replicatePlugin.ts
@@ -0,0 +1,17 @@
+import { constructResponse } from "../actions/respond.ts";
+
+/**
+ * Replicates the given model 'n' times and adds it to an array that gets returned
+ * @param times the amount of times you want this object replicated
+ * @param model the model name to replicate WITH the extension
+ */
+export function replicate(times: number, data: object): Array<object>{
+    let items: any = [];
+    for(var i = 0; i < times; i++){
+        items.push(data)
+    }
+    return items ;
+}
+
+
+// TODO: add load method to load a single different model in another one.

f06f40b0668174a4151e6bed87bddc88966a18b6
duart38 <mr.codercuracao@gmail.com>
2020-07-22 02:42:06 +0200

    Added async replication support

--- actions/respond.ts
+++ actions/respond.ts
@@ -4,12 +4,16 @@
  * @param model interface to use to construct response data
  */
 export async function constructResponse(model: string): Promise<Object> {
-    let m  = await import(`../models/${model}?${Math.random()}.ts`);
+  let m = await import(`../models/${model}?${Math.random()}.ts`);
+  if (typeof m[Object.keys(m)[0]] == "function") {
+    return await m[Object.keys(m)[0]]();
+  } else {
     return m[Object.keys(m)[0]];
+  }
 }
-export function constructHeaders(req: any){
-    let headers = req.headers;
-    headers.append("Access-Control-Allow-Origin", "*")
-    headers.append("Access-Control-Allow-Headers", "*")
-    return headers;
+export function constructHeaders(req: any) {
+  let headers = req.headers;
+  headers.append("Access-Control-Allow-Origin", "*");
+  headers.append("Access-Control-Allow-Headers", "*");
+  return headers;
 }
--- plugins/replicatePlugin.ts
+++ plugins/replicatePlugin.ts
@@ -2,6 +2,7 @@ import { constructResponse } from "../actions/respond.ts";
 
 /**
  * Replicates the given model 'n' times and adds it to an array that gets returned
+ * Note that the data that is replicated does not re-evaluate the object thus making the values static and the same across iterations
  * @param times the amount of times you want this object replicated
  * @param model the model name to replicate WITH the extension
  */
@@ -9,6 +10,24 @@ export function replicate(times: number, data: object): Array<object> {
   let items: any = [];
   for (var i = 0; i < times; i++) {
     items.push(data);
+  }
+  return items;
+}
+
+/**
+ * Replicates the given model 'n' times and adds it to an array that gets returned
+ * This is the asynchronous way of replicating and requires your model to be wrapped in an async function
+ * @example export var Test = async ()=> Object({ ... }); // object constructor here was used for simplicity
+ * @param times 
+ * @param data 
+ */
+export async function replicateAsync(
+  times: number,
+  modelName: string,
+): Promise<Array<object>> {
+  let items: any = [];
+  for (var i = 0; i < times; i++) {
+    items.push(await constructResponse(modelName));
   }
   return items;
 }
